3e3c846af8b6e79692020210e6341543eb5e74b7
huhubun <nongzc@outlook.com>
2025-02-28 21:30:51 +0800

    开启AOT和裁剪

--- src/Bammemo.Web/Bammemo.Web.Client/SourceGenerationContext.cs
+++ src/Bammemo.Web/Bammemo.Web.Client/SourceGenerationContext.cs
@@ -5,19 +5,20 @@ using System.Text.Json.Serialization;
 
 namespace Bammemo.Web.Client;
 
-//[JsonSourceGenerationOptions(WriteIndented = true)]
-//[JsonSerializable(typeof(string))]
-//[JsonSerializable(typeof(TextUrlSetting))]
-//[JsonSerializable(typeof(SlipDetailDto))]
-//[JsonSerializable(typeof(ListSlipDto))]
-//[JsonSerializable(typeof(ListSlipResponse))]
-//[JsonSerializable(typeof(GetSlipByIdResponse))]
-//[JsonSerializable(typeof(CreateSlipResponse))]
-//[JsonSerializable(typeof(UpdateSlipRequest))]
-//[JsonSerializable(typeof(GetSlipTimesResponse))]
-//[JsonSerializable(typeof(GetSlipTagsResponse))]
-//[JsonSerializable(typeof(GetSettingByKeyResponse))]
-//[JsonSerializable(typeof(BatchGetSettingByKeyResponse))]
-//internal partial class SourceGenerationContext : JsonSerializerContext
-//{
-//}
+[JsonSourceGenerationOptions(WriteIndented = true)]
+[JsonSerializable(typeof(string))]
+[JsonSerializable(typeof(TextUrlSetting))]
+[JsonSerializable(typeof(List<TextUrlSetting>))]
+[JsonSerializable(typeof(SlipDetailDto))]
+[JsonSerializable(typeof(ListSlipDto))]
+[JsonSerializable(typeof(ListSlipResponse))]
+[JsonSerializable(typeof(GetSlipByIdResponse))]
+[JsonSerializable(typeof(CreateSlipResponse))]
+[JsonSerializable(typeof(UpdateSlipRequest))]
+[JsonSerializable(typeof(GetSlipTimesResponse))]
+[JsonSerializable(typeof(GetSlipTagsResponse))]
+[JsonSerializable(typeof(GetSettingByKeyResponse))]
+[JsonSerializable(typeof(BatchGetSettingByKeyResponse))]
+internal partial class SourceGenerationContext : JsonSerializerContext
+{
+}
