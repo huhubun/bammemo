c204b10a9e593d6d27dba0d183c1f89cbf34414e
huhubun <nongzc@outlook.com>
2025-03-02 02:50:20 +0800

    配置移动到appsettings.json中

---
+++ src/Bammemo.Service.Server/Options/BammemoOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Bammemo.Service.Server.Configurations;
+
+public class BammemoOptions
+{
+    public const string Position = "Bammemo";
+
+    public required string ConnectionString { get; set; }
+    public required string ApiUrl { get; set; }
+    public string? WebUrl { get; set; }
+}
