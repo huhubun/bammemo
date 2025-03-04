7a077ffbb469238072ebb261cff780c452301736
huhubun <nongzc@outlook.com>
2025-03-04 13:12:55 +0800

    由于appsettings.json对于webassembly来说属于静态资源，无法从k8s里覆盖修改，现在将配置单独放到bammemo.json中单独加载

--- src/Bammemo.Web/Bammemo.Web.Client/Program.cs
+++ src/Bammemo.Web/Bammemo.Web.Client/Program.cs
@@ -1,15 +1,12 @@
-using Bammemo.Service.Options;
+using Bammemo.Web.Client.Options;
 using Bammemo.Web.Client.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-var bammemoOptions = builder.Configuration.GetSection(BammemoWebClientOptions.Position).Get<BammemoWebClientOptions>() ?? throw new NullReferenceException(nameof(BammemoWebClientOptions));
+var bammemoOptions = await AddBammemoWebClientOptionsAsync(builder);
 
 builder.Services.AddFluentUIComponents();
-
-builder.Services.Configure<BammemoWebClientOptions>(
-    builder.Configuration.GetSection(BammemoWebClientOptions.Position));
 
 builder.Services.AddAutoMapper(typeof(Program));
 
@@ -19,3 +16,21 @@ builder.Services.AddScoped<ICommonSlipService, CommonSlipService>();
 builder.Services.AddScoped<ICommonSettingService, CommonSettingService>();
 
 await builder.Build().RunAsync();
+
+static async Task<BammemoWebClientOptions> AddBammemoWebClientOptionsAsync(WebAssemblyHostBuilder builder)
+{
+    var http = new HttpClient()
+    {
+        BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+    };
+
+    using var response = await http.GetAsync(BammemoWebClientOptions.FileName);
+    using var stream = await response.Content.ReadAsStreamAsync();
+
+    builder.Configuration.AddJsonStream(stream);
+
+    var section = builder.Configuration.GetSection(BammemoWebClientOptions.Position);
+    builder.Services.Configure<BammemoWebClientOptions>(section);
+
+    return section.Get<BammemoWebClientOptions>() ?? throw new NullReferenceException(nameof(BammemoWebClientOptions));
+}
