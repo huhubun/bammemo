807b5e2c623749c82a47c79a0ab11464f2051999
huhubun <nongzc@outlook.com>
2025-03-27 18:34:51 +0800

    #20 处理 Forwarded headers

--- src/Bammemo.Web/Bammemo.Web/Program.cs
+++ src/Bammemo.Web/Bammemo.Web/Program.cs
@@ -11,6 +11,7 @@ using Bammemo.Web.Components;
 using Bammemo.Web.Identities;
 using Bammemo.Web.MinimalApis;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
@@ -25,6 +26,10 @@ using Scalar.AspNetCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddEnvironmentVariables();
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+});
 
 // Web
 builder.Services.AddRazorComponents()
@@ -123,6 +128,8 @@ builder.Services.AddScoped<ICommonAnalyticsService, CommonAnalyticsService>();
 builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
+
+app.UseForwardedHeaders();
 
 #if DEBUG
 app.UseWebAssemblyDebugging();
